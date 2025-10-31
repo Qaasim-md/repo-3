3885fe293efe0ef98d67246e8f8b1316612f1645
Qaasim-md <qaasimdavids1@gmail.com>
2025-10-31 09:43:18 +0200

    add exercise 3

---
+++ array.js
@@ -0,0 +1,17 @@
+let colors = [];
+
+colors.push('red');
+colors.push('black');
+colors.push('blue');
+
+let numbers =[20, 40, 60, 80, 100];
+
+colors.pop();
+
+colors.unshift('green');
+
+console.log("colors array lenght", colors.length);
+console.log("numbers array length", numbers.length);
+
+console.log("Colors",colors);
+console.log("Numbers",numbers);
